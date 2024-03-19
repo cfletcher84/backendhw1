febc3b5ca02321afae8797cd106fa115a8431f5e
cfletcher84 <chris@soldbyfletch.com>
2024-03-19 09:09:15 -0500

    Swapping values updated

--- homework.py
+++ homework.py
@@ -55,3 +55,11 @@ num1 = num1 - num2
 print("Num1 = ",num1)
 print("Num2 = ", num2)
 print(num1 == num2)
+
+
+# Swapping values
+num1 = 10
+num2 = 3
+print(num1, num2)
+num1, num2 = num2, num1
+print(num1, num2)
