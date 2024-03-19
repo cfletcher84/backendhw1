534d3fa9d9edcb7751f54c4040ad51153193d27a
cfletcher84 <chris@soldbyfletch.com>
2024-03-18 22:26:44 -0500

    initial commit

---
+++ homework.py
@@ -0,0 +1,52 @@
+# Homwork Queston 1 task 1
+weather = "sunny"
+if weather == "sunny":
+    print("Wear sunglasses!")
+else:
+    print("Take an umbrella!")
+
+# Homework Queston 1 Task 2
+user_mood = input("How do you feel today? sad or happy? ")
+if user_mood == "sad":
+    print("I hope your day gets better!")
+if user_mood == "happy":
+    print("That's great to hear!")
+
+# Homework Question 2
+# pi_value = <span class="hljs-number">3.14</span>
+# user_age = <span class="hljs-number">25</span>
+# user_location = <span class="hljs-string">"New York"</span>
+# MAX_LIMIT = <span class="hljs-number">1000</span>
+
+# Homework Queston 3
+
+variable_a = type(("Hello, World!"))
+print(variable_a)
+variable_b = type(23)
+print(variable_b)
+variable_c = type(3.14)
+print(variable_c)
+variable_d = type(True)
+print(variable_d)
+
+# Homeqwork Question 4 Task 1
+apple_cost = 1.50
+milk_cost = 5.00
+bread_cost = 2.50
+total_cost = (apple_cost + milk_cost + bread_cost)
+print("The total cost for your items is: ", total_cost)
+
+# Homework Queston 4 Task 2
+starting_balance = 10000
+rate = .07
+interest_gained = (starting_balance * rate)
+ending_balance = (starting_balance + interest_gained)
+print("Your ending balace is: ", ending_balance)
+
+# Homework Question 5 (This is hard)
+num1 = 3
+num2 = 10
+print(num1 == num2)
+print(num1, num2)
+num1 = num2 
+print(num1, num2)
